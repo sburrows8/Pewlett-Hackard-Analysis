b1884b522fc04ac6a33710d063a200deeef75484
Samantha Burrows <sburrows291@gmail.com>
2022-08-04 21:03:00 -0400

    final commit

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -15,7 +15,8 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
+SELECT DISTINCT ON (emp_no)
+	emp_no,
 	first_name,
 	last_name,
 	title
@@ -30,7 +31,8 @@ GROUP BY title
 ORDER BY COUNT DESC;
 
 -- Deliverable 2: The Employees Eligible for the Mentorship Program
-SELECT DISTINCT ON (e.emp_no,de.emp_no,ti.emp_no) e.emp_no,
+SELECT DISTINCT ON (e.emp_no,de.emp_no,ti.emp_no) 
+	e.emp_no,
 	e.first_name,
 	e.last_name,
 	e.birth_date,
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pewlett-Hackard-Analysis
+
+## Overview
+The purpose of this assignment was to assist Bobby by finding all retiring employees organized by title as well as identifying all employees who were eligible for the mentorship program. 
+
+### Results
